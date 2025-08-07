d2ce5340519fa49e96c01aee750ecdc410ebeab2
Avinash <avinashdubey8780@gmail.com>
2025-08-07 18:48:58 +0530

    made changes to authMiddleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models.User");
 
-const authToken = (req, res, next) => {
+const authToken = async(req, res, next) => {
   const authHeader = req.headers.authorization;
   
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
@@ -11,25 +12,19 @@ const authToken = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    const user = await User.findById(decoded.id).select("-password");
+
+    if(!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+    
+    req.user = user;
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid or expired token" });
   }
 };
 
-const isAdmin = (req, res, next) => {
-  if (req.user.role !== 'admin') {
-    return res.status(403).json({ message: "Forbidden: Admin access only" });
-  }
-  next();
-};
 
-const isAgent = (req, res, next) => {
-  if (req.user.role !== 'agent') {
-    return res.status(403).json({ message: "Forbidden: Agent access only" });
-  }
-  next();
-};
 
-module.exports = { authToken, isAdmin, isAgent };
+module.exports = { authToken };
